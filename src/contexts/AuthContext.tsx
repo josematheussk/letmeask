433658b74bf020bd53ca971eb12072bb1a9d1fe4
José Santos <josematheussk@gmail.com>
2021-09-13 19:45:34 -0300

    second commit

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useEffect, useState} from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import { auth, firebase } from "../services/firebase";
 
 type User = {
@@ -6,7 +6,7 @@ type User = {
   name: string;
   avatar: string;
 }
-  
+
 type AuthContextType = {
   user: User | undefined;
   signInWithGoogle: () => Promise<void>;
@@ -18,59 +18,55 @@ type AuthContextProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextType);
 
-export function AuthContextProvider(props: AuthContextProviderProps){
-
-    
+export function AuthContextProvider(props: AuthContextProviderProps) {
   const [user, setUser] = useState<User>();
 
+  
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(user =>{
-      if (user){
-        const {displayName, photoURL, uid } = user;
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        const { displayName, photoURL, uid } = user
         
-        if(!displayName || !photoURL){
+        if (!displayName || !photoURL) {
           throw new Error('Missing information from Google Account.');
         }
-      
+        
         setUser({
           id: uid,
           name: displayName,
           avatar: photoURL
         })
-        
       }
     })
-
+    
     return () => {
       unsubscribe();
     }
-
   }, [])
 
-  async function signInWithGoogle(){
+  async function signInWithGoogle() {
     const provider = new firebase.auth.GoogleAuthProvider();
-
+    
     const result = await auth.signInWithPopup(provider);
-
-        if(result.user){
-          const {displayName, photoURL, uid } = result.user;
-        
-          if(!displayName || !photoURL){
-            throw new Error('Missing information from Google Account.');
-          }
-        
-          setUser({
-            id: uid,
-            name: displayName,
-            avatar: photoURL
-          })
-        }
-
-  } 
-
-    return(
-        <AuthContext.Provider value={{ user, signInWithGoogle}}>    
-            {props.children}
-        </AuthContext.Provider>
-    );
+    
+    if (result.user) {
+      const { displayName, photoURL, uid } = result.user
+      
+      if (!displayName || !photoURL) {
+        throw new Error('Missing information from Google Account.');
+      }
+      
+      setUser({
+        id: uid,
+        name: displayName,
+        avatar: photoURL
+      })
+    }
+  }
+  
+  return (
+    <AuthContext.Provider value={{ user, signInWithGoogle }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
 }
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,72 +1,64 @@
-import { FormEvent, useState } from 'react';
-import {Link, useHistory} from 'react-router-dom'
-import illustrationImg from '../assets/images/illustration.svg';
+import { FormEvent, useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
+
+import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg';
-import '../styles/auth.scss';
+
 import { Button } from '../components/Button';
-import { useContext } from 'react';
-import { AuthContext } from '../contexts/AuthContext';
 import { database } from '../services/firebase';
+import { useAuth } from '../hooks/useAuth';
 
-export function NewRoom()  {
-    const { user } = useContext(AuthContext);
-    const history = useHistory();
-    const [newRoom, setNewRoom] = useState('');
+import '../styles/auth.scss';
 
-    async function handleCreateRoom(event: FormEvent) {
-        event.preventDefault();
-        console.log(newRoom);
+export function NewRoom() {
+  const { user } = useAuth()
+  const history = useHistory()
+  const [newRoom, setNewRoom] = useState('');
 
-        if (newRoom.trim() === ''){
-            return;
-        }
+  async function handleCreateRoom(event: FormEvent) {
+    event.preventDefault();
 
-        const roomRef = database.ref('rooms');
-
-        const firebaseRoom = await roomRef.push({
-            title: newRoom,
-            authorID: user?.id,
-        });
-
-        history.push(`/rooms/${firebaseRoom.key}`);
-
+    if (newRoom.trim() === '') {
+      return;
     }
 
-    return(
-        <div id="page-auth">
-            <aside>
-                <img src={illustrationImg} alt="Ilustrção simbolizando perguntas e respostas"/>
-                <strong>Crie salas de Q&amp;A ao-vivo</strong>
-                <p>Tire as dúvidas da sua audiência em tempo-real.</p>
-            </aside>
-            <main>
-                <div className="main-content">
-                    <img src={logoImg} alt="Letmeask" />
-                    <h2>Crie uma nova sala</h2>
-                    <form onSubmit={handleCreateRoom}>
-                        
-                        <input 
-                            type="text"
-                            placeholder="Nome da sala" 
-                            onChange={event => setNewRoom(event.target.value)}
-                            value={newRoom}
-                        />
+    const roomRef = database.ref('rooms');
 
-                        <h1></h1>
+    const firebaseRoom = await roomRef.push({
+      title: newRoom,
+      authorId: user?.id,
+    })
 
-                        <Button type="submit">
-                            Entrar na sala
-                        </Button>
+    history.push(`/rooms/${firebaseRoom.key}`)
+  }
 
-                    </form>
-                        <p>
-                            Quer entrar em uma sala existente? 
-                            <Link to="/">
-                                Clique aqui!
-                            </Link>    
-                        </p>
-                </div>
-            </main>
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <h2>Criar uma nova sala</h2>
+          <form onSubmit={handleCreateRoom}>
+            <input 
+              type="text"
+              placeholder="Nome da sala"
+              onChange={event => setNewRoom(event.target.value)}
+              value={newRoom}
+            />
+            <Button type="submit">
+              Criar sala
+            </Button>
+          </form>
+          <p>
+            Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
+          </p>
         </div>
-    )
+      </main>
+    </div>
+  )
 }
